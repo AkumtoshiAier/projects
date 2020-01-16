3ff1f77f6a8af735e1ef948093fcf0070fa61b2a
Akum <funkyakum@gmail.com>
2020-01-16 14:04:50 +0530

    Ready for deployment

---
+++ client/build/precache-manifest.e477520e8f7f00d4cb51c8da6540fdfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806cfe5f2f804898afcba606ed1de048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8bb47d52f131064a341",
+    "url": "/static/css/main.372c4e10.chunk.css"
+  },
+  {
+    "revision": "6040ca07f6c2e5a5b432",
+    "url": "/static/js/2.ea16039e.chunk.js"
+  },
+  {
+    "revision": "28ca8f53298bbc4f809d4b81f70af65f",
+    "url": "/static/js/2.ea16039e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8bb47d52f131064a341",
+    "url": "/static/js/main.041543d0.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "714cbf7e9a06e84ea721299040ef3954",
+    "url": "/static/media/showcase.714cbf7e.jpg"
+  }
+]);
